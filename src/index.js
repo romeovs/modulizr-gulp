e8d3b9e85ccbd34fdf33113c4e758be7e368dec7
Romeo Van Snick <romeovs@gmail.com>
2015-04-07 12:27:05 +0200

    add repo

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import modernizr from 'modernizr'
+import gutil     from 'gulp-util'
+import es        from 'event-stream'
+
+export default function(config) {
+  // extract filename
+  var filename = config.filename || 'modernizr.js';
+  delete config.filename;
+
+  var building = false;
+  return es.readable(function(ct, callback) {
+    if ( !building ) {
+      building = true;
+      modernizr.build(config, function (result) {
+        this.emit('data', new gutil.File({
+          cwd      : ''
+        , base     : ''
+        , path     : filename
+        , contents : new Buffer(result)
+        }));
+        this.emit('end')
+        callback();
+      }.bind(this));
+    }
+  });
+};
